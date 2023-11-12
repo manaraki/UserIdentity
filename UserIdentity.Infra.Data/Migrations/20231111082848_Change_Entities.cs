eef8203b975bb30e6607d365af0bc3320948849b
maryam <maryam.anaraki@gmail.com>
2023-11-12 10:21:31 +0330

    Change Database

---
+++ UserIdentity.Infra.Data/Migrations/20231111082848_Change_Entities.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserIdentity.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Change_Entities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InsertTime",
+                table: "RolePermissions");
+
+            migrationBuilder.DropColumn(
+                name: "IsRemoved",
+                table: "RolePermissions");
+
+            migrationBuilder.DropColumn(
+                name: "RemoveTime",
+                table: "RolePermissions");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateTime",
+                table: "RolePermissions");
+
+            migrationBuilder.RenameColumn(
+                name: "UserRoleId",
+                table: "UserRoles",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Username",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "UserRoles",
+                newName: "UserRoleId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "InsertTime",
+                table: "RolePermissions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRemoved",
+                table: "RolePermissions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RemoveTime",
+                table: "RolePermissions",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateTime",
+                table: "RolePermissions",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "RolePermissions",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "InsertTime", "IsRemoved", "RemoveTime", "UpdateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), false, null, null });
+
+            migrationBuilder.UpdateData(
+                table: "RolePermissions",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "InsertTime", "IsRemoved", "RemoveTime", "UpdateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), false, null, null });
+
+            migrationBuilder.UpdateData(
+                table: "RolePermissions",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "InsertTime", "IsRemoved", "RemoveTime", "UpdateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), false, null, null });
+
+            migrationBuilder.UpdateData(
+                table: "RolePermissions",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "InsertTime", "IsRemoved", "RemoveTime", "UpdateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), false, null, null });
+
+            migrationBuilder.UpdateData(
+                table: "RolePermissions",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "InsertTime", "IsRemoved", "RemoveTime", "UpdateTime" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), false, null, null });
+        }
+    }
+}
