e68119997735168d56e8fa90644b918ef3a3a96c
maryam <maryam.anaraki@gmail.com>
2023-11-13 13:04:13 +0330

    Change region Repository

--- UserIdentity.Infra.Ioc/DependencyContainer.cs
+++ UserIdentity.Infra.Ioc/DependencyContainer.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UserIdentity.Application.Interfaces;
 using UserIdentity.Application.Services;
+using UserIdentity.Domain.Entities.User;
 using UserIdentity.Infra.Data.Contexts;
 using UserIdentity.Infra.Data.Interfaces;
 using UserIdentity.Infra.Data.Repositories;
@@ -16,22 +17,23 @@ namespace UserIdentity.Infra.Ioc
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            #region Services
             services.AddScoped<IRegisterUserService, RegisterUserService>();
             services.AddScoped<IAuthenticateUserService, AuthenticateUserService>();
             services.AddScoped<IAddUserService, AddUserService>();
             services.AddScoped<IUserService, UserService>();
-
             services.AddScoped<ITokenService, TokenService>();
+            #endregion
 
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<IRoleRepository, RoleRepository>();
-            services.AddScoped<IPermissionRepository, PermissionRepository>();
-            services.AddScoped<IRolePermissionRepository, RolePermissionRepository>();
-            services.AddScoped<IUserRoleRepository, UserRoleRepository>(); 
-              
-
+            #region Repository
+            services.AddScoped<IGenericRepository<User>, GenericRepository<User>>();
+            services.AddScoped<IGenericRepository<Role>, GenericRepository<Role>>();
+            services.AddScoped<IGenericRepository<UserRole>, GenericRepository<UserRole>>();
+            services.AddScoped<IGenericRepository<Permission>, GenericRepository<Permission>>();
+            services.AddScoped<IGenericRepository<RolePermission>, GenericRepository<RolePermission>>();
+            #endregion
 
         }
-        
+
     }
 }
